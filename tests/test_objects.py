2eaa3b5ec1f8749e3d29e53a3185904bfd56dc5f
poseitrader <posei.dawn@outlook.com>
2025-11-09 22:53:51 +0530

    Posei Experiments: Improve compatibility with latest Rust versions

--- tests/test_objects.py
+++ tests/test_objects.py
@@ -61,3 +61,5 @@ def test_pickling_fail():
 # Posei Experiments: Code update - 20260101154219
 
 # Posei Experiments: Code update - 20260101154224
+
+# Posei Experiments: Code update - 20260101154229
