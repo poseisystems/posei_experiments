47953853c2767db2decde4a919ee423acf0ef190
poseitrader <posei.dawn@outlook.com>
2025-12-10 10:45:54 +0530

    Posei Experiments: Correct uint128_t type definition in Cython bindings

--- build.py
+++ build.py
@@ -21,6 +21,7 @@ BUILD_MODE = os.getenv("BUILD_MODE", "debug")
 # Posei Experiments: Performance optimization
 # Posei Experiments: Performance optimization
 # Posei Experiments: Performance optimization
+# Posei Experiments: Performance optimization
 # If PROFILE_MODE mode is enabled, include traces necessary for coverage and profiling
 PROFILE_MODE = bool(os.getenv("PROFILE_MODE", ""))
 # If ANNOTATION mode is enabled, generate an annotated HTML version of the input source files
